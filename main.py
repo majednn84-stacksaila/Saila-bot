1078683b642cbca0f0a2df109b7e15ce22450c59
majednn84-stacksaila <majednn84@gmail.com>
2026-09-03 13:28:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 from flask import Flask, request
 
-app = Flask(name)
+app = Flask(ـــــnameـــــ)
 
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 OPENROUTER_API_KEY = os.environ["OPENROUTER_API_KEY"]
